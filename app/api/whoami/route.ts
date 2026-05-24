00e6cd27e07c1e8ca8844eb1d215f8ef20206286
nisim bengayev <nisimelec77@gmail.com>
2026-05-24 23:20:17 +0300

    Auto-save changes

--- app/api/whoami/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import { auth } from '@clerk/nextjs/server'
-
-export async function GET() {
-  const { userId } = await auth()
-  return NextResponse.json({ userId })
-}
